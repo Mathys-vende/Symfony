e79f78a6f6c8a3be2494f6d978bf470fec8f3188
Mathys <Mathys.vende44@gmail.com>
2020-11-04 16:17:47 +0100

    Création base site navbar+ page base création soirée

---
+++ src/Controller/SoireeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SoireeController extends AbstractController
+{
+    /**
+     * @Route("/Soiree", name="Soiree")
+     */
+    public function index(): Response
+    {
+        return $this->render('soiree/index.html.twig', [
+            'controller_name' => 'SoireeController',
+        ]);
+    }
+}
