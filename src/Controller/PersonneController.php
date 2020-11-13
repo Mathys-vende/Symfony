eb572a925381fbf6dcb33ab07b1c097412c3157c
Mathys <Mathys.vende44@gmail.com>
2020-11-13 17:14:52 +0100

    modif apparence site

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -123,7 +123,7 @@ class PersonneController extends AbstractController
 
 
     /**
-     * @Route("/chatons/supprimer/{id}/{idSoiree}", name="supprimer_personne")
+     * @Route("/personne/supprimer/{id}/{idSoiree}", name="supprimer_personne")
      */
 
     public function supprimer($id, Soiree $idSoiree, Request $request){
